bdb85e6c8522dddb5a8ca22d70dd269ea42083d9
Lil-C0der <howtousegmai1@gmail.com>
2020-11-03 17:04:45 +0800

    docs: fix a typo

--- src/App.tsx
+++ src/App.tsx
@@ -24,37 +24,21 @@ function App() {
         </Col>
       </Row> */}
 
-      <Menu
-        onClick={() => {}}
-        onClose={function noRefCheck() {}}
-        onOpen={function noRefCheck() {}}
-        onSelect={function noRefCheck() {}}
-        selectedIndex="item_4"
-        trigger="click"
-        vertical={false}
-      >
-        <Menu.Item disabled={false} index="item_1">
-          item 1
-        </Menu.Item>
+      <Menu selectedIndex="item_4" trigger="click">
+        <Menu.Item index="item_1">item 1</Menu.Item>
         <Menu.Item disabled index="item_2">
           item 2
         </Menu.Item>
         <Menu.Submenu index="item_3" title="item 3">
-          <Menu.Item disabled={false} index="item_3_1">
-            item 3-1
-          </Menu.Item>
+          <Menu.Item index="item_3_1">item 3-1</Menu.Item>
           <Menu.Item disabled index="item_3_2">
             item 3-2
           </Menu.Item>
           <Menu.Submenu index="item_3_3" title="item 3-3">
-            <Menu.Item disabled={false} index="item_3_3_1">
-              item 3-3-1
-            </Menu.Item>
+            <Menu.Item index="item_3_3_1">item 3-3-1</Menu.Item>
           </Menu.Submenu>
         </Menu.Submenu>
-        <Menu.Item disabled={false} index="item_4">
-          item 4
-        </Menu.Item>
+        <Menu.Item index="item_4">item 4</Menu.Item>
       </Menu>
     </div>
   );
