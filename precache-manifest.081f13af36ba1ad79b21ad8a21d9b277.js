59dd7bf66276c634e944ad383191295494eb2eae
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-18 01:54:56 +0000

    Deploy Lil-C0der/woo-ui-react to github.com/Lil-C0der/woo-ui-react.git:gh-pages

---
+++ precache-manifest.081f13af36ba1ad79b21ad8a21d9b277.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87f396a698115c33dee201d9be0098de",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.a4312593af1f1a72fead.bundle.js"
+  },
+  {
+    "url": "runtime~main.a4312593af1f1a72fead.bundle.js"
+  },
+  {
+    "revision": "d6bb0188c25208eddc3d",
+    "url": "static/css/main.43f6e06b.chunk.css"
+  },
+  {
+    "url": "vendors~main.a4312593af1f1a72fead.bundle.js"
+  },
+  {
+    "url": "vendors~main.a4312593af1f1a72fead.bundle.js.LICENSE.txt"
+  }
+]);
